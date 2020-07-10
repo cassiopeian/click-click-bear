d98de21e5b68493a1d47f9c297e85d5f4cfe0dca
cassiopeia <cassiopeia.neely@gmail.com>
2020-07-09 21:27:37 -0400

    created js file
    » Started making variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// this converts the HTML elements into usable variables
+const shrub1 = document.getElementById('bush-one');
+const shrub2 = document.getElementById('bush-two');
+const shrub3 = document.getElementById('bush-three');
+const startButton = document.getElementById('start');
+
+// these are the images that will appear when the doors open/close
+const foxSurprise = '../images/ccb-fox.png';
+const skunkSurprise = '../images/ccb-skunk.png';
+const bearSurprise = '../images/ccb-bear.png';
+const undisturbedShrub = '../images/green-shrub.png';
