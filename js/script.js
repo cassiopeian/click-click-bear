bc22b99b03ae60b6118930c58ebc53b190b33126
cassiopeia <cassiopeia.neely@gmail.com>
2020-07-14 20:25:54 -0400

    troubleshooting... still
    » Tried renaming things, to see if I'd made typos. Didn't solve anything.
    » Added new console.log checks to the shrub1.onclick. currentlyPlaying returns a "true" value, which is good. But the isClicked checker also returns a value of "true" every time the shrub is clicked. This should not happen.

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ const shrub3 = document.getElementById('bush3');
 const startButton = document.getElementById('start');
 
 // these images will appear when the shrubs are poked/left alone
+const bearSurprise = 'images/ccb-favicon-01.png';
 const foxSurprise = 'images/ccb-fox.png';
 const skunkSurprise = 'images/ccb-skunk.png';
-const bearSurprise = 'images/ccb-favicon-01.png';
-const undisturbedShrub = 'images/green-shrub.png';
+const greenShrub = 'images/green-shrub.png';
 
 // these variables are used in the randomShrubGenerator function
-let numUndisturbedShrubs = 3;
+let numGreenShrubs = 3;
 let pokedShrub1;
 let pokedShrub2;
 let pokedShrub3;
@@ -19,37 +19,37 @@ let pokedShrub3;
 // the click functions, below, depend on this being true; the shrubs become unclickable once the gameOver function triggers the false state
 let currentlyPlaying = true;
 
+// this checks to see if the bear shrub has been clicked
+const isBear = (shrub) => {
+    if (shrub.src === bearSurprise) {
+      return true;
+    } else {
+      return false;
+    }
+};
+
 // this prevents players from clicking each shrub more than once
-let isClicked = (thingy) => {
-    if (thingy.src === undisturbedShrub) {
+const isClicked = (shrub) => {
+    if (shrub.src === greenShrub) {
       return false;
     } else {
       return true;
     }
-  };
-
-// this checks to see if the bear shrub has been clicked
-let isBear = (thingy) => {
-  if (thingy.src === bearSurprise) {
-    return true;
-  } else {
-    return false;
-  }
 };
 
 // this decreases the number of undisturbed shrubs and checks to see if the game is over
-const chooseShrub = (thingy) => {
-  numUndisturbedShrubs--;
-  if (numUndisturbedShrubs === 0) {
+const chooseShrub = (shrub) => {
+  numGreenShrubs--;
+  if (numGreenShrubs === 0) {
     gameOver('win');
-  } else if (isBear(thingy)) {
+  } else if (isBear(shrub)) {
     gameOver();
   } 
 };
 
 //this randomly assigns the image that will appear when the shrubs are poked
 const randomShrubGenerator = () => {
-    let shrubOption = Math.floor(Math.random() * numUndisturbedShrubs);
+    const shrubOption = Math.floor(Math.random() * numGreenShrubs);
     if (shrubOption === 0) {
       pokedShrub1 = bearSurprise;
       pokedShrub2 = skunkSurprise;
@@ -66,30 +66,55 @@ const randomShrubGenerator = () => {
 };
 
 // these click functions reveal the randomized animals if it's true that the game should still be ongoing and the door hasn't already been clicked
+
+// shrub1.onclick = () => {
+//     // if (currentlyPlaying === true && !isClicked(shrub1)) {
+//     if (currentlyPlaying === true) {
+//       shrub1.src = pokedShrub1;
+//       chooseShrub(shrub1);
+//     }
+//     // shrub1.src = pokedShrub1;
+//     console.log('You clicked shrub 1!');
+// };
+// shrub2.onclick = () => {
+//     // if (currentlyPlaying === true && !isClicked(shrub2)) {
+//     if (currentlyPlaying === true) {
+//       shrub2.src = pokedShrub2;
+//       chooseShrub(shrub2);
+//     }
+//     console.log('You clicked shrub 2!');
+// };
+// shrub3.onclick = () => {
+//     // if (currentlyPlaying === true && !isClicked(shrub3)) {
+//     if (currentlyPlaying === true) {
+//       shrub3.src = pokedShrub3;
+//       chooseShrub(shrub3);
+//     }
+//     console.log('You clicked shrub 3!');
+// };
+
 shrub1.onclick = () => {
-    // if (currentlyPlaying === true && !isClicked(shrub1)) {
-    if (currentlyPlaying === true) {
+    console.log('You clicked shrub 1!');
+    console.log(currentlyPlaying);
+    console.log(isClicked(shrub1));
+    if (currentlyPlaying === true && !isClicked(shrub1)) {
       shrub1.src = pokedShrub1;
       chooseShrub(shrub1);
     }
-    // shrub1.src = pokedShrub1;
-    console.log('You clicked shrub 1!');
 };
 shrub2.onclick = () => {
-    // if (currentlyPlaying === true && !isClicked(shrub2)) {
-    if (currentlyPlaying === true) {
+    console.log('You clicked shrub 2!');
+    if (currentlyPlaying === true && !isClicked(shrub2)) {
       shrub2.src = pokedShrub2;
       chooseShrub(shrub2);
     }
-    console.log('You clicked shrub 2!');
 };
 shrub3.onclick = () => {
-    // if (currentlyPlaying === true && !isClicked(shrub3)) {
-    if (currentlyPlaying === true) {
+    console.log('You clicked shrub 3!');
+    if (currentlyPlaying === true && !isClicked(shrub3)) {
       shrub3.src = pokedShrub3;
       chooseShrub(shrub3);
     }
-    console.log('You clicked shrub 3!');
 };
 
 // the startButton click event relies on the startRound function
@@ -101,10 +126,10 @@ startButton.onclick = () => {
 
 // this resets the game values
 const startRound = () => {
-    shrub1.src = undisturbedShrub;
-    shrub2.src = undisturbedShrub;
-    shrub3.src = undisturbedShrub;
-    numUndisturbedShrubs = 3;
+    shrub1.src = greenShrub;
+    shrub2.src = greenShrub;
+    shrub3.src = greenShrub;
+    numGreenShrubs = 3;
     startButton.innerHTML = 'Good Luck!';
     currentlyPlaying = true;
     randomShrubGenerator();
